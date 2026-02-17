d11ae86abefd39350da4e54b533768dac25084ff
thomas <thomas.kratz@eiswind.de>
2026-02-17 15:11:47 +0100

    fix tsconfig

--- src/messages/model.ts
+++ src/messages/model.ts
@@ -1,18 +1,25 @@
 import {Elysia, t} from "elysia";
 
 export const messageDTO = t.Object({
-        text: t.String({readOnly: true}),
-        createdAt: t.Optional(t.String({
-            required: false,
-            format: 'date-time',
-            readOnly: true
-        })),
-        id: t.String({uuid: true, readOnly: true}),
-        likes: t.Number({readOnly: true})
-    })
+    text: t.String({readOnly: true}),
+    createdAt: t.Optional(t.String({
+        required: false,
+        format: 'date-time',
+        readOnly: true
+    })),
+    id: t.String({uuid: true, readOnly: true}),
+    likes: t.Number({readOnly: true})
+})
+
+
+export const createOrEditMessageDTO = t.Object({
+    text: t.String({readOnly: true}),
+    likes: t.Number({readOnly: true})
+})
 
 
 export const messageModel = new Elysia()
     .model({
         MessageDTO: messageDTO,
+        CreateOrEditMessageDTO: createOrEditMessageDTO
     })
