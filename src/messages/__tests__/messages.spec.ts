652ae0cc1ca554b3115f8f9dc569808eb105fb83
thomas <thomas.kratz@eiswind.de>
2026-02-17 16:02:33 +0100

    add update & delete

--- src/messages/__tests__/messages.spec.ts
+++ src/messages/__tests__/messages.spec.ts
@@ -44,4 +44,93 @@ describe("Messages", () => {
 
     })
 
+    it('should save a message', async () => {
+
+        const testMessage = {text: 'hello world', likes: 0}
+
+        const response = await app
+            .handle(new Request('http://localhost/api/messages', {
+                method: 'POST',
+                headers: [['Content-Type', 'application/json']],
+                body: JSON.stringify(testMessage)
+            }))
+
+        expect(response.status).toEqual(201)
+
+        const json = await response.json()
+
+        expect(json.text).toEqual(testMessage.text)
+        expect(json.likes).toEqual(testMessage.likes)
+        expect(json.id).toBeTruthy()
+        expect(json.createdAt).toBeTruthy()
+
+        const rows = await db.select().from(message).execute()
+        expect(rows.length).toEqual(1)
+        const record = rows[0]!!
+
+        expect(record.text).toEqual(testMessage.text)
+        expect(record.likes).toEqual(testMessage.likes)
+        expect(record.id).toBeTruthy()
+        expect(record.createdAt).toBeTruthy()
+    })
+
+
+    it('should update a message', async () => {
+
+        const testMessage = {text: 'hello world', likes: 0}
+        const updatedMessage = {text: 'hello hello', likes: 1}
+
+        let rows = await db.insert(message).values(testMessage).returning()
+        expect(rows.length).toEqual(1)
+        const record = rows[0]!!
+
+
+        const response = await app
+            .handle(new Request(`http://localhost/api/messages/${record.id}`, {
+                method: 'PUT',
+                headers: [['Content-Type', 'application/json']],
+                body: JSON.stringify(updatedMessage)
+            }))
+
+        expect(response.status).toEqual(200)
+
+        const json = await response.json()
+
+        expect(json.text).toEqual(updatedMessage.text)
+        expect(json.likes).toEqual(updatedMessage.likes)
+        expect(json.id).toEqual(record.id)
+        expect(json.createdAt).toEqual(record.createdAt?.toISOString())
+
+        rows = await db.select().from(message).execute()
+        expect(rows.length).toEqual(1)
+        const newRecord = rows[0]!!
+
+        expect(newRecord.text).toEqual(updatedMessage.text)
+        expect(newRecord.likes).toEqual(updatedMessage.likes)
+        expect(newRecord.id).toEqual(record.id)
+        expect(newRecord.createdAt).toEqual(record.createdAt)
+    })
+
+    it('should delete a message', async () => {
+
+        const testMessage = {text: 'hello world', likes: 0}
+
+        let rows = await db.insert(message).values(testMessage).returning()
+        expect(rows.length).toEqual(1)
+        const record = rows[0]!!
+
+
+        const response = await app
+            .handle(new Request(`http://localhost/api/messages/${record.id}`, {
+                method: 'DELETE'
+            }))
+
+        expect(response.status).toEqual(200)
+
+
+
+        rows = await db.select().from(message).execute()
+        expect(rows.length).toEqual(0)
+    })
+
 })
--- src/messages/messages.ts
+++ src/messages/messages.ts
@@ -1,8 +1,9 @@
-import {Elysia,  t } from "elysia"
+import {Elysia, t} from "elysia"
 import {dbPlugin} from "../db/plugin.js";
 import {message} from "../db/schema.js";
 import type {DrizzleDb} from "../db/types.js";
 import {messageModel} from "./model.js";
+import {eq} from "drizzle-orm";
 
 
 export const createMessagesPlugin = (options: { db: DrizzleDb }) => {
@@ -13,7 +14,7 @@ export const createMessagesPlugin = (options: { db: DrizzleDb }) => {
         .get('',
             async ({db, status}) => {
                 const rows = await db().select().from(message)
-                const isoDateRows = rows.map(r =>  ({ ...r, createdAt: r.createdAt?.toISOString() }))
+                const isoDateRows = rows.map(r => ({...r, createdAt: r.createdAt?.toISOString()}))
                 return status(200, isoDateRows)
             },
             {
@@ -25,5 +26,66 @@ export const createMessagesPlugin = (options: { db: DrizzleDb }) => {
                 }
             }
         )
+        .post('', async ({db, body, set, status}) => {
+            const result = await db().insert(message).values(body).returning()
+            if (result && result.length === 1) {
+                const record = result[0]
+                if (record) {
+                    return status(201, {...record, createdAt: record.createdAt?.toISOString()})
+                }
+            }
+            set.status = 500
+            return
+
+        }, {
+            body: t.Ref('CreateOrEditMessageDTO'),
+            response: {
+                201: t.Ref('MessageDTO'),
+                500: t.Void()
+            },
+            detail: {
+                tags: ['messages'],
+                operationId: 'saveMessage',
+                summary: 'Create a new message'
+            }
+        })
+        .put('/:id', async ({db, body, set, status, params: {id}}) => {
+            const result = await db().update(message).set(body).where(eq(message.id, id)).returning()
+            if (result && result.length === 1) {
+                const record = result[0]
+                if (record) {
+                    return {...record, createdAt: record.createdAt?.toISOString()}
+                }
+            }
+            set.status = 404
+            return
+
+        }, {
+            body: t.Ref('CreateOrEditMessageDTO'),
+            response: {
+                200: t.Ref('MessageDTO'),
+                404: t.Void()
+            },
+            detail: {
+                tags: ['messages'],
+                operationId: 'updateMessage',
+                summary: 'Update a message'
+            }
+        })
+        .delete('/:id', async ({db, status, set, params: {id}}) => {
+            const result = await db().delete(message).where(eq(message.id, id)).returning()
+            if (result && result.length === 1) {
+                set.status = 200
+            } else {
+                set.status = 404
+            }
+            return
+        }, {
+            detail: {
+                tags: ['messages'],
+                operationId: 'deleteMessageById',
+                summary: 'Delete a message'
+            }
+        })
 
 }
