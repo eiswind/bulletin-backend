a55f8637b42a86307b8d88a0a0ccde6226c5c116
thomas <thomas.kratz@eiswind.de>
2026-05-06 18:12:41 +0200

    knip

--- src/auth/model.ts
+++ src/auth/model.ts
@@ -1,12 +1,12 @@
 import {Elysia, t} from "elysia";
 
-export const userDTO = t.Object({
+const userDTO = t.Object({
     username: t.String(),
     password: t.String()
 })
 
 
-export const credentialsDTO = t.Object({
+const credentialsDTO = t.Object({
     username: t.String(),
     password: t.String()
 })
